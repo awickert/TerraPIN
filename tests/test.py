9f149a200412cdab0a9c4522af02547451b34c52
Andy Wickert <andy@northernwidget.com>
2016-04-01 16:15:48 -0500

    'Reload' to allow rapid testing of code

--- tests/test.py
+++ tests/test.py
@@ -1,29 +1,62 @@
 #! /usr/bin/python
 
-from terrapin import *
+import terrapin
+
+reload(terrapin)
 
 from matplotlib import pyplot as plt
 
-self = Terrapin()
+self = terrapin.Terrapin()
 self.initialize()
 self.topoPlot()
 self.z_ch = -60.
 self.updateFluvialTopo_z()
 self.topoPlot()
+self.z_ch -= 1.
+self.updateFluvialTopo_z()
+self.updateFluvialTopo_z()
+self.topoPlot()
+self.z_ch -= 1.
+self.updateFluvialTopo_z()
+
+self.topoPlot()
 self.z_ch -= 10.
 self.updateFluvialTopo_z()
+
 self.topoPlot()
-self.z_ch += 50.
+self.z_ch += 15
 self.updateFluvialTopo_z()
+
 self.topoPlot()
+self.z_ch -= 1
+
+
+
+self.updateFluvialTopo_z()
+
+self.topoPlot()
+"""
 self.z_ch -= 10
 self.updateFluvialTopo_z()
 self.topoPlot()
+
 self.z_ch += 20
+self.updateFluvialTopo_z()
+self.topoPlot()
+
+self.z_ch -= 2
+self.updateFluvialTopo_z()
+self.topoPlot()
+
+self.z_ch -= 2
 self.updateFluvialTopo_z()
 self.topoPlot()
 self.z_ch -= 2
 self.updateFluvialTopo_z()
+self.topoPlot()
+self.z_ch -= 10
+self.updateFluvialTopo_z()
+"""
 
 self.topoPlot('ko-')
 plt.show()
